5e41990a5de35d4258c99f1c7b5b21f91150f9c3
nino-porcino <nino.porcino@gmail.com>
2019-05-18 10:12:14 +0200

    implement keyboard

--- README.md
+++ README.md
@@ -6,3 +6,14 @@ HOW TO USE
 To run the emulator, simply open to the link: 
 [nippur72.github.io/ChildZ-emu](https://nippur72.github.io/ChildZ-emu/). 
 
+
+KEYBOARD
+========
+
+A standard ASCII keyboard is emulated. 
+
+- `Backspace` and `Delete` are swapped (they give `127` and `8` respectively)
+- Left and Right Cursor keys are mapped to `<` and `>` (avalaible also via `Ctrl`+`,` and `Ctrl`+`.`)
+- Ascii codes below 32 can be produced by `Ctrl` + letter or `Alt` + letter (because the browser prevent using some Ctrl keys)
+- `NULL` is `Ctrl` + `0` or `Ctrl` + `@`
+- `Ctrl` + `Alt` + `P` powers on/off the machine
--- keyboard.js
+++ keyboard.js
@@ -27,11 +27,62 @@ function keyDown(e) {
    */
 
    // ALT+P is power OFF/ON
-   if(e.code == "KeyP" && e.altKey) {
+   if(e.code == "KeyP" && e.altKey && e.ctrlKey) {
       power();
       e.preventDefault();
       return;
    }   
+
+   //console.log(`e.key=${e.key} e.code=${e.code}`);
+
+   let k = 0;
+
+        if(e.code === 'Backspace')   k = 127;
+   else if(e.code === 'Tab')         k = 9;
+   else if(e.code === 'Enter')       k = 13;
+   else if(e.code === 'NumpadEnter') k = 13;
+   else if(e.code === 'Escape')      k = 27;
+   else if(e.code === 'Delete')      k = 8;
+   else if(e.code === 'ArrowLeft')   k = '<'.charCodeAt(0);
+   else if(e.code === 'ArrowRight')  k = '>'.charCodeAt(0);
+
+   // control codes
+   if((e.ctrlKey || e.altKey)) {      
+           if(e.key.toUpperCase() == '0' || e.key.toUpperCase() == '@') k = 0;      
+      else if(e.key.toUpperCase() == 'A') k = 1;      
+      else if(e.key.toUpperCase() == 'B') k = 2;      
+      else if(e.key.toUpperCase() == 'C') k = 3;      
+      else if(e.key.toUpperCase() == 'D') k = 4;      
+      else if(e.key.toUpperCase() == 'E') k = 5;      
+      else if(e.key.toUpperCase() == 'F') k = 6;      
+      else if(e.key.toUpperCase() == 'G') k = 7;      
+      else if(e.key.toUpperCase() == 'H') k = 8;      
+      else if(e.key.toUpperCase() == 'I') k = 9;      
+      else if(e.key.toUpperCase() == 'J') k = 10;      
+      else if(e.key.toUpperCase() == 'K') k = 11;      
+      else if(e.key.toUpperCase() == 'L') k = 12;      
+      else if(e.key.toUpperCase() == 'M') k = 13;      
+      else if(e.key.toUpperCase() == 'N') k = 14;      
+      else if(e.key.toUpperCase() == 'O') k = 15;      
+      else if(e.key.toUpperCase() == 'P') k = 16;      
+      else if(e.key.toUpperCase() == 'Q') k = 17;      
+      else if(e.key.toUpperCase() == 'R') k = 18;      
+      else if(e.key.toUpperCase() == 'S') k = 19;      
+      else if(e.key.toUpperCase() == 'T') k = 20;      
+      else if(e.key.toUpperCase() == 'U') k = 21;      
+      else if(e.key.toUpperCase() == 'V') k = 22;      
+      else if(e.key.toUpperCase() == 'W') k = 23;      
+      else if(e.key.toUpperCase() == 'X') k = 24;      
+      else if(e.key.toUpperCase() == 'Y') k = 25;      
+      else if(e.key.toUpperCase() == 'Z') k = 26;      
+      else if(e.key.toUpperCase() == '[') k = 27; 
+      else if(e.key.toUpperCase() == '\\') k = 28;      
+      else if(e.key.toUpperCase() == ']') k = 29;      
+      else if(e.key.toUpperCase() == '^' || e.key.toUpperCase() == 'ì') k = 30;      
+      else if(e.key.toUpperCase() == '_' || e.key.toUpperCase() == '-') k = 31;      
+      else if(e.key.toUpperCase() == ',')  k = '<'.charCodeAt(0);
+      else if(e.key.toUpperCase() == '.')  k = '>'.charCodeAt(0);
+   }
 
    if(e.code == "ShiftLeft" || 
       e.code == "ShiftRight" || 
@@ -78,10 +129,15 @@ function keyDown(e) {
    */  
    
    // console.log("down",e);
-   
-   key_pressed_port_cd = e.keyCode;
-   cpu.interrupt(false, 0x70); // trigger interrupt mode 2 (im 2), jumps at $0070
 
+   if(k !== 0) key_pressed_port_cd = k;
+   else {
+      if(e.key.length === 1) {
+         key_pressed_port_cd = e.key.toUpperCase().charCodeAt(0);
+      }
+   }
+   
+   cpu.interrupt(false, 0x70); // trigger interrupt mode 2 (im 2), jumps at $0070
    e.preventDefault();         
 }
 
@@ -89,7 +145,52 @@ function keyUp(e) {
    e.preventDefault();
 }
 
+function keyPress(e) { 
+   let k=0;
+
+   // control codes
+   if(e.ctrlKey && !e.shiftKey) {      
+            if(e.key.toUpperCase() == '0') k = 0;      
+      else if(e.key.toUpperCase() == 'A') k = 1;      
+      else if(e.key.toUpperCase() == 'B') k = 2;      
+      else if(e.key.toUpperCase() == 'C') k = 3;      
+      else if(e.key.toUpperCase() == 'D') k = 4;      
+      else if(e.key.toUpperCase() == 'E') k = 5;      
+      else if(e.key.toUpperCase() == 'F') k = 6;      
+      else if(e.key.toUpperCase() == 'G') k = 7;      
+      else if(e.key.toUpperCase() == 'H') k = 8;      
+      else if(e.key.toUpperCase() == 'I') k = 9;      
+      else if(e.key.toUpperCase() == 'J') k = 10;      
+      else if(e.key.toUpperCase() == 'K') k = 11;      
+      else if(e.key.toUpperCase() == 'L') k = 12;      
+      else if(e.key.toUpperCase() == 'M') k = 13;      
+      else if(e.key.toUpperCase() == 'N') k = 14;      
+      else if(e.key.toUpperCase() == 'O') k = 15;      
+      else if(e.key.toUpperCase() == 'P') k = 16;      
+      else if(e.key.toUpperCase() == 'Q') k = 17;      
+      else if(e.key.toUpperCase() == 'R') k = 18;      
+      else if(e.key.toUpperCase() == 'S') k = 19;      
+      else if(e.key.toUpperCase() == 'T') k = 20;      
+      else if(e.key.toUpperCase() == 'U') k = 21;      
+      else if(e.key.toUpperCase() == 'V') k = 22;      
+      else if(e.key.toUpperCase() == 'W') k = 23;      
+      else if(e.key.toUpperCase() == 'X') k = 24;      
+      else if(e.key.toUpperCase() == 'Y') k = 25;      
+      else if(e.key.toUpperCase() == 'Z') k = 26;      
+      else if(e.key.toUpperCase() == '\\') k = 28;      
+      else if(e.key.toUpperCase() == ']') k = 29;      
+      else if(e.key.toUpperCase() == '^' || e.key.toUpperCase() == 'ì') k = 30;      
+      else if(e.key.toUpperCase() == '_' || e.key.toUpperCase() == '-') k = 31;      
+      else if(e.key.toUpperCase() == ',')  k = '<'.charCodeAt(0);
+      else if(e.key.toUpperCase() == '.')  k = '>'.charCodeAt(0);
+   }
+
+   if(k!=0) e.preventDefault();
+}
+
 const element = document; //.getElementById("canvas");
 
 element.onkeydown = keyDown;
 element.onkeyup = keyUp;
+element.onkeypress = keyPress;
+
