a992090cd7b649363d6efd3beb73294ed11ad213
nino-porcino <nino.porcino@gmail.com>
2019-05-29 20:20:30 +0200

    use asmproc syntax

--- software/msbasic/m.bat
+++ software/msbasic/m.bat
@@ -1,2 +1,2 @@
-node C:\Users\Nino\Desktop\USB\GitHub\asmproc\asmproc.js -i childz-basic.lm -o childz-basic.asm --target z80
+node C:\Users\Nino\Desktop\USB\GitHub\asmproc\asmproc.js -i childz-basic.lm -o childz-basic.asm --target z80asm
 yaza childz-basic.asm --lst --sym
